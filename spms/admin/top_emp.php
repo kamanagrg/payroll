ff5b06b3d818226aa8f382c7fafad4cf24d8cbb9
kamanagrg <ariesgurung17@gmail.com>
2023-03-08 13:06:49 +0545

    payroll

---
+++ spms/admin/top_emp.php
@@ -0,0 +1,103 @@
+
+<div class="card">
+    <div class="card-header d-flex justify-content-between">
+        
+        <h3 class="card-title">Top Paid Employees</h3>
+        <div class="card-tools align-middle">
+            Month &nbsp <select>
+                <option value="jan">January</option>
+                <option value="feb">February</option>
+                <option value="march">March</option>
+                <option value="april">April</option>
+                <option value="m">May</option>
+                <option value="ju">June</option>
+                <option value="jl">July</option>
+                <option value="au">August</option>
+                <option value="feb">September</option>
+                <option value="feb">October</option>
+                <option value="feb">November</option>
+                <option value="feb">December</option>
+                <select> 
+        </div>
+    </div>
+    <div class="card-body">
+        <table class="table table-hover table-striped table-bordered">
+            <colgroup>
+                <col width="5%">
+                <col width="15%">
+                <col width="20%">
+                <col width="20%">
+            </colgroup>
+            <thead>
+                <tr>
+                    <th class="text-center p-0">S.N</th>
+                    <th class="text-center p-0">Name</th>
+                    <th class="text-center p-0">Designation</th>
+                    <th class="text-center p-0">Salary</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+                <tr>
+                    <td class="text-center p-0"><?php echo 1; ?></td>
+                    <td class="py-0 px-1"><?php echo "Usha Karki" ?></td>
+                    <td class="py-0 px-1"><?php echo "Sr. Accountant" ?></td>
+                    <td class="py-0 px-1"><?php echo "Rs.40000" ?></td>                    
+                </tr>
+                <tr>
+                    <td class="text-center p-0"><?php echo 2; ?></td>
+                    <td class="py-0 px-1"><?php echo "Rose Baral" ?></td>
+                    <td class="py-0 px-1"><?php echo "Front Desk Officer"?></td>
+                    <td class="py-0 px-1"><?php echo "Rs.25300" ?></td>                    
+                </tr>
+                <tr>
+                    <td class="text-center p-0"><?php echo 3; ?></td>
+                    <td class="py-0 px-1"><?php echo " Ram Lama" ?></td>
+                    <td class="py-0 px-1"><?php echo "Secondary English"?></td>
+                    <td class="py-0 px-1"><?php echo "Rs.21000" ?></td>                    
+                </tr>
+
+                <tr>
+                    <td class="text-center p-0"><?php echo 4; ?></td>
+                    <td class="py-0 px-1"><?php echo "Mamata Rana" ?></td>
+                    <td class="py-0 px-1"><?php echo "Secondary English " ?></td>
+                    <td class="py-0 px-1"><?php echo "Rs.15000" ?></td>                    
+                </tr>
+                <tr>
+                    <td class="text-center p-0"><?php echo 5; ?></td>
+                    <td class="py-0 px-1"><?php echo "Monika Budathoki" ?></td>
+                    <td class="py-0 px-1"><?php echo "Lower Secondary Science Teacher" ?></td>
+                    <td class="py-0 px-1"><?php echo "Rs.25000" ?></td>                    
+                </tr>
+
+                <tr>
+                    <td class="text-center p-0"><?php echo 6; ?></td>
+                    <td class="py-0 px-1"><?php echo "Mohani Maya Chaudhary" ?></td>
+                    <td class="py-0 px-1"><?php echo "Secondary Computer Teacher" ?></td>
+                    <td class="py-0 px-1"><?php echo "Rs.30000" ?></td>                    
+                </tr>
+                <tr>
+                    <td class="text-center p-0"><?php echo 7; ?></td>
+                    <td class="py-0 px-1"><?php echo "Sam Lama" ?></td>
+                    <td class="py-0 px-1"><?php echo " Secondary English" ?></td>
+                    <td class="py-0 px-1"><?php echo "Rs.27000" ?></td>                    
+                </tr>
+                <tr>
+                    <td class="text-center p-0"><?php echo 8; ?></td>
+                    <td class="py-0 px-1"><?php echo "Bimal Dahal" ?></td>
+                    <td class="py-0 px-1"><?php echo "Security Guard" ?></td>
+                    <td class="py-0 px-1"><?php echo "Rs.20000" ?></td>                    
+                </tr>
+                <tr>
+                    <td colspan="3" class="text-center"><b>Total salary paid <b></td>
+                    <td><b>Rs.203300 <b></td>
+                   
+                </tr>
+
+
+            </tbody>
+            
+        </table>
+       
+    </div>
+</div>
